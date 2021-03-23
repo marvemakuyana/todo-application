04d42b2803f5ec9962b3b5d71ffeaf0b9cb40e7e
marvemakuyana <59795303+marvemakuyana@users.noreply.github.com>
2021-03-23 21:06:36 +0200

    Create a React Application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require('./hello_react')
+require('./components/TodoApp')
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
 
